3ac868a3e61e806b53635533b76ea220c980fbb7
hifzhan28 <zhanprtm2005@gmail.com>
2025-05-25 12:43:59 +0700

    selesai

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/cekstatus', function () {
+    return view('cekstatus');
+})->name('cekstatus');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
